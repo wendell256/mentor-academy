5756fb7fefc6f1fd86a6dab2fc773bc607a4cf53
Wendell Enamorado <wendell256@gmail.com>
2025-06-18 22:05:06 -0300

    feat: fix import statement for Stats component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from '@/components/hero';
 import MentorIntro from '@/components/mentor-intro';
-import Stats from '@/components/stats.';
+import Stats from '@/components/stats';
 
 export default function Home() {
   return (
--- components/stats.tsx
+++ components/stats.tsx
@@ -3,30 +3,28 @@ import React from 'react';
 export default function Stats() {
   return (
     <section className='bg-[#A5D0AA]/20'>
-      <div className='mx-auto grid max-w-7xl grid-cols-2 md:grid-cols-4 px-4 text-primary'>
+      <div className='mx-auto max-w-7xl flex items-start px-4 gap-x-8 text-primary'>
         <div className='relative flex items-start justify-center gap-4 py-8'>
           <span className='absolute right-[-15px] top-1/2 -translate-y-1/2 h-[56px] w-px bg-gray-300'></span>
-          <span className='text-[42px] font-bold text-[#FF7751] leading-none'>
+          <span className='text-2xl md:text-5xl font-bold text-[#FF7751] leading-none'>
             12 Month
           </span>
-          <span className='text-gray-500 leading-tight'>
+          <span className='text-gray-500 text-sm md:text-base leading-tight mt-1'>
             Mentorship
             <br />
             Journey
           </span>
         </div>
         <div className='flex items-start justify-center gap-4 py-8'>
-          <span className='text-[42px] font-bold text-[#FF7751] leading-none'>
+          <span className='text-2xl md:text-5xl font-bold text-[#FF7751] leading-none'>
             100%
           </span>
-          <span className='text-gray-500 leading-tight'>
+          <span className='text-gray-500 text-sm md:text-base leading-tight mt-1'>
             Systemized
             <br />
             Setup
           </span>
         </div>
-        <div className='hidden md:block'></div>
-        <div className='hidden md:block'></div>
       </div>
     </section>
   );
